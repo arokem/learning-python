4bb43c3a47caa0b2b8c5c658f2c04dceab39d733
arokem <arokem@gmail.com>
2015-01-10 17:38:22 -0800

    Some changes to testing materials.

--- testing/test_circle.py
+++ testing/test_circle.py
@@ -1,6 +1,8 @@
 import circle
 import numpy as np
+import numpy.testing as npt
 
 def test_area():
-    assert circle.area(1) == np.pi
-    assert np.abs(circle.area(np.sqrt(np.pi))-(np.pi ** 2)) < 10e-15
+    npt.assert_equal(circle.area(1), np.pi)
+    npt.assert_almost_equal(circle.area(np.sqrt(np.pi)) , (np.pi ** 2))
+
